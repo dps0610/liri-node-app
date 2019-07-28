74577dc3435b1a79ac3c8fb98848d8e6b5321add
dps0610 <dps0610@hotmail.com>
2019-07-27 23:21:31 -0400

    Functions concertThis, spotifyThis, and movieThis working.

--- liri.js
+++ liri.js
@@ -3,6 +3,8 @@ var Spotify = require('node-spotify-api');
 var keys = require("./key");
 var moment = require('moment');
 var spotify = new Spotify(keys.spotify);
+const axios = require('axios');
+var fs = require("fs");
 
 switch(process.argv[2]) {
 case "concert-this":
@@ -21,21 +23,25 @@ case "do-what-it-says":
     doWhatItSays();
     break;
 }
+
 // Searches artist name in Bands in Town api
 function concertThis(){
     //grab band to search
-    var artist = process.argv.slice(3).join(" ");
-    console.log(artist);
+    var artistName = process.argv.slice(3).join(" ");
+    console.log(artistName);
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
     //run the search
-
-    //log the Artist, Venue, Date of event
-    // console.log("Artist: " + );
-    // console.log("Venue: " + );
-    // console.log("Date: " + );
+    axios.get(queryUrl)
+    .then(function(response){
+        //log the Artist, Venue, Date of event
+        console.log("Artist: " + artistName);
+        console.log("City: " + response.data[0].venue.city);
+        console.log("Date: "+ moment(response.data[0].datetime).format("MM/DD/YYYY"));
+    });
 }
 
 // Searches for songs on spotify
-function spotifyThis(){
+function spotifyThis(song){
     //grab song to search
     var songName = process.argv.slice(3).join(" ");
     console.log("Searched: " + songName);
@@ -56,14 +62,32 @@ function movieThis(){
     //grab movie name
     var movieName = process.argv.slice(3).join(" ");
     console.log(movieName);
-    //run the search
-
-    //log return 'pretty'
-
-    //give it a default if nothing entered
-
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=2b44ca8d";
+    //run the search with default
+    if (movieName === ""){
+        axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&apikey=2b44ca8d")
+        .then(function(response){
+            console.log("Title: " + response.data.Title);
+            console.log("Release Date: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Freshness: " + response.data.Ratings[1].Value);
+            console.log("Produced In: " + response.data.Country);
+            console.log("Language(s): " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Title: " + response.data.Actors);
+        })
+    }else {
+        axios.get(queryUrl)
+        .then(function(response){
+            console.log("Title: " + response.data.Title);
+            console.log("Release Date: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Freshness: " + response.data.Ratings[1].Value);
+            console.log("Produced In: " + response.data.Country);
+            console.log("Language(s): " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Title: " + response.data.Actors);
+        });
+    }
 }
 
-function doWhatItSays(){
-    //run spotifyThis with information entered from random.txt
-}
